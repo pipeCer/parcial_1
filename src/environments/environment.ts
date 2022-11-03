e94261437a0df9858344dd8326dec5a2c3c89437
Andrés Cerquera <a.cerquera@uniandes.edu.co>
2022-11-03 11:45:47 -0500

    feature: updated apiUrl env variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://back-museums-uniandes-g1.herokuapp.com/api/v1/',
+  apiUrl: 'https://gist.githubusercontent.com/josejbocanegra/17bb8c76405e43655d551a90800c8a81/raw/d41b4acc3457e51e7533fad6d5e9925ee9676457/202212_MISW4104_Grupo1.json/',
 };
 
 /*
