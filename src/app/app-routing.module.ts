597aaf112843a0abd822a91822666fc0f634e737
Andrés Cerquera <a.cerquera@uniandes.edu.co>
2022-11-03 11:45:16 -0500

    feature: added app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MuseumDetailComponent} from "./views/museum-detail/museum-detail.component";
-import { ExhibitionDetailComponent } from "./views/exhibition-detail/exhibition-detail.component";
-import {HomeComponent} from "./views/home/home.component";
+import {VehiclesListComponent} from "./views/vehicles-list/vehicles-list.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'museums/:id', component: MuseumDetailComponent},
-  {path: 'museums/:museumId/exhibitions/:exhibitionId', component: ExhibitionDetailComponent}
+  {path: '', component: VehiclesListComponent},
 ];
 
 @NgModule({
