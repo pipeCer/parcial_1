ed74347a4e06cf3d8ba799da7a876a407527024d
Andrés Cerquera <a.cerquera@uniandes.edu.co>
2022-11-03 11:44:41 -0500

    feature: updated main app to works with views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterOutlet} from "@angular/router";
+import {VehiclesListModule} from "./views/vehicles-list/vehicles-list.module";
+import {AppRoutingModule} from "./app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    HttpClientModule,
+    AppRoutingModule,
+    VehiclesListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
