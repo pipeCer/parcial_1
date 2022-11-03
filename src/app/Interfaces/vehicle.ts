5f2565a059ff67e3b7f2b70011379092fb7f4a10
Andrés Cerquera <a.cerquera@uniandes.edu.co>
2022-11-03 10:10:29 -0500

    feature: added Vehicle interface

---
+++ src/app/Interfaces/vehicle.ts
@@ -0,0 +1,14 @@
+export module Vehicle {
+
+  export interface Request {
+    id: number;
+    marca: string;
+    linea: string;
+    referencia: string;
+    modelo: number;
+    kilometraje: number;
+    color: string;
+    imagen: string;
+  }
+
+}
