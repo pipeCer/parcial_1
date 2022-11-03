9355de20b021102231274b2ffb87ccd714407e37
Andrés Cerquera <a.cerquera@uniandes.edu.co>
2022-11-03 14:44:46 -0500

    feature: added total vehicles per brand

--- src/app/components/vehicles/vehicles.component.ts
+++ src/app/components/vehicles/vehicles.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Vehicle} from "../../Interfaces/vehicle";
 import {VehiclesListService} from "../../services/vehicles-list.service";
 
@@ -9,15 +9,33 @@ import {VehiclesListService} from "../../services/vehicles-list.service";
 })
 export class VehiclesComponent implements OnInit {
   public vehicles: Vehicle.Request[] = [];
+  public totalBrands: (string)[] = [];
+
   constructor(private vehiclesService: VehiclesListService) {
     this.retrieveVehicles();
   }
 
   ngOnInit(): void {
   }
+
   retrieveVehicles(): void {
     this.vehiclesService.getVehicles().subscribe((data: Vehicle.Request[]) => {
       this.vehicles = data;
+      this.getTotalBrands();
     })
+  }
+
+  getTotalBrands(): void {
+    const brands = this.vehicles.map((vehicle: Vehicle.Request) => vehicle.marca);
+    let brandCounted: string[] = []
+    const brandsCount = brands.map((brand) => {
+      const brandFiltered = brands.filter(element => (element == brand && brandCounted.indexOf(brand) == -1))
+      if (brandFiltered.length) {
+        brandCounted.push(brand);
+        return `${brand}: ${brandFiltered.length}`
+      }
+      return ""
+    })
+    this.totalBrands = brandsCount.filter(element => element !== "")
   }
 }
